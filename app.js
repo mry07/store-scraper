20492fa8db521d616b515f7d0bda8becfcb967aa
MrY07 <vaveglobal@VAVEGLOBALs-MacBook-Pro.local>
2023-05-17 11:13:29 +0700

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import AppStoreScraper from "app-store-scraper";
 import GooglePlayScraper from "google-play-scraper";
 
 const app = Express();
-const port = 3000;
+const port = 3001;
 
 app.use(Express.json());
 
